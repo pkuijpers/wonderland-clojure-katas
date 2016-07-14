0b9c3e2d34464157f25d32ca45184454aa35b8f0
Pieter Kuijpers <pieter@pi-q.nl>
2016-07-14 19:40:39 +0200

    Finished wonderland-number.

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,16 @@
 (ns wonderland-number.finder)
 
+(defn hasAllTheSameDigits? [n1 n2]
+  (let [s1 (set (str n1))
+        s2 (set (str n2))]
+    (= s1 s2)))
+
+(defn is-wonderland-number? [n]
+  (and (hasAllTheSameDigits? n (* 2 n))
+       (hasAllTheSameDigits? n (* 3 n))
+       (hasAllTheSameDigits? n (* 4 n))
+       (hasAllTheSameDigits? n (* 5 n))
+       (hasAllTheSameDigits? n (* 6 n))))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (filter is-wonderland-number? (range 100000 (/ 999999 6)))))
