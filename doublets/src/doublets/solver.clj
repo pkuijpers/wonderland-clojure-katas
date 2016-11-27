1da5b2d24897cba9f4a99f07a4af070701c36fdb
Pieter Kuijpers <pieter@pi-q.nl>
2016-11-27 12:47:13 +0100

    Finished doublets

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,26 @@
                (slurp)
                (read-string)))
 
+(defn link? [word1 word2]
+  (and
+    (= (count word1) (count word2))
+    (let [letter-pairs (map vector word1 word2)
+          diffs (filter #(not= (first %) (last %)) letter-pairs)]
+      (= 1 (count diffs)))))
+
+(defn links[doublet]
+  (let [lw (last doublet)
+        equal-length-words (filter #(= (count %) (count lw)) words)]
+    (map #(conj doublet %)
+         (filter #(and
+                    (not-any? (partial = %) doublet)
+                    (link? lw %))
+                 equal-length-words))))
+
 (defn doublets [word1 word2]
-  "make me work")
+  (let [doublet
+        (some #(when (= word2 (last %)) %)
+              (tree-seq #(not= (last %) word2)
+                        links
+                        [word1]))]
+    (or doublet [])))
